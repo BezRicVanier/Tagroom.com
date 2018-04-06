d69887f08e76eb9cf0f764690812329f842fc70f
BezRicVanier <36086158+BezRicVanier@users.noreply.github.com>
2018-04-06 09:42:27 -0400

    Add files via upload

---
+++ base.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Test1</title>
+</head>	
+<body>
+
+<form action="program.php" method="post">
+    Please enter a sentence:<br><input type="text" name="channel0Title" value=""><br>
+    <input type="submit" id ="submitButton" value="Submit">
+</form>
+	
+</body>
+</html>
