96739d171bf22fb4b453f737fbb13f450a1237d4
BezRicVanier <36086158+BezRicVanier@users.noreply.github.com>
2018-05-04 15:12:34 -0400

    Add files via upload

--- apple.py
+++ apple.py
@@ -1,7 +1,7 @@
 import nltk
 from nltk.tokenize import word_tokenize
 file1 = open("C:/xampp/htdocs/WebInput.txt", "r")
-file2 = open("C:\/xampp/htdocs/TaggedSentence.txt", "w")
+file2 = open("C:/xampp/htdocs/TaggedSentence.txt", "a+")
 shortcake = str(file1.read())
 usertext = word_tokenize(shortcake)
 taggedsentence =(nltk.pos_tag(usertext))
